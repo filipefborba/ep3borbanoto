2fa6fef49851e401edc9c75e109b8185cbda391e
LucaNoto <luca.ribeiro.noto@gmail.com>
2016-04-16 18:57:27 -0300

    Update classejogo.py

--- classejogo.py
+++ classejogo.py
@@ -1,2 +1,14 @@
 class Jogo:
   def __init__:
+
+
+  def iniciar(self):
+    self.window.mainloop()
+        
+  def apertou(self):
+    self.button.configure(text="O")
+    self.button.configure(font="Calibri 100 bold")
+        
+  def sorteia_iniciante():
+    a=random.randint(0,1)
+    return a
