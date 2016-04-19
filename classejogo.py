f94d80eafefeef488d286c3cdb52cd12c93aa23c
Filipe Foschini Borba <filipefborba@gmail.com>
2016-04-19 09:25:29 -0300

    Updates nas classes

--- classejogo.py
+++ classejogo.py
@@ -1,15 +1,12 @@
 import numpy as np
+import classetabuleiro
 
-class Jogo(Tabuleiro):
+class Jogo():
   def __init__(self):
+    self.tabuleiro = tabuleiro.classetabuleiro()
 
   def iniciar(self):
     self.window.mainloop()
-    
-  def apertou(self):
-    #Evento: necessário para registrar a jogada, add na lista e verificar se ganhou.
-    self.button.configure(text="O")
-    self.button.configure(font="Calibri 100 bold")
     
   def sorteia_iniciante():
     a = random.randint(0,1)
@@ -19,42 +16,20 @@ class Jogo(Tabuleiro):
     else:
         self.player = self.player[1]
     
-  def verifica_ganhador():
-    #Verificação das possíveis vitórias
-    for i in range(3):
-    
-    #Verificar quem ganha, -1 indica que o jogo está em andamento
-    if 
-        print("Deu Velha!")
-        return 0
-    elif listaX in vitoria:
-        print("O jogador 1 (X) venceu!")
-        return 1
-    elif listaO in vitoria:
-        print("O jogador 2 (O) venceu!")
-        return 2
-    else:
-        return -1
-
-""" Método para verificar se o jogo acabou. A função deve
-retornar 0 em caso de empate, 1 caso o X seja o vencedor, 2 caso o O seja o
-vencedor ou -1 caso contrário. ListaX,ListaO = posições do X/O no tabuleiro."""
-
-def recebe_jogada(linha, coluna):
+  def recebe_jogada(linha, coluna):
     verifica_ganhador() = verifica
-    matrizX = np.zeros([3,3])
-    matrizO = np.zeros([3,3])
+    matriz = np.array([None,None,None][None,None,None][None,None,None])
     
     #Registrar jogada
     while True:
         if verifica == -1:
-            if matrizX[linha][coluna] == 0:
+            if matriz[linha][coluna] == None:
                 if self.player == 0:
-                    self.button.configure(text=self.player[0])
-                    matrizX[linha][coluna] = 1
+                    self.clicou(linha,coluna)
+                    matriz[linha][coluna] = 1
                     self.player = self.player[1]
                 elif self.player == 1:
-                    self.button.configure(text=self.player[1])
+                    self.clicou(linha,coluna)
                     matrizO[linha][coluna] = 0
                     self.player = self.player[0]
         if verifica >= 0:
@@ -63,14 +38,27 @@ def recebe_jogada(linha, coluna):
 """Método para receber e registrar uma jogada.
 Deve também alternar os jogadores. Não há retorno de valores."""
 
+  def verifica_ganhador():
+    #Verificação das possíveis vitórias
+    #Verificar quem ganha, -1 indica que o jogo está em andamento
+  
+    #Colocar sleep e depos limpa_jogadas? ##################################
+        print("Deu Velha!")
+        return 0
+        print("O jogador 1 (X) venceu!")
+        return 1
+        print("O jogador 2 (O) venceu!")
+        return 2
+    else:
+        return -1
+        
 def limpa_jogadas():
     #Reiniciar os botões, cliques, etc.
     #Podemos fazer um botão pra isso
     verifica_ganhador() = verifica
     
     #Reiniciar registro de jogadas
-    matrizX.clear()
-    matrizO.clear()
+    matriz = np.array([None,None,None][None,None,None][None,None,None])
     
     #Reiniciar botões
     
