41bdce085fa1eff40ec91abedb4e95d7614447dc
LucaNoto <luca.ribeiro.noto@gmail.com>
2016-04-19 15:57:24 -0300

    Update classetabuleiro.py
    Arrumei as fontes e o tamanho do X e O

--- classetabuleiro.py
+++ classetabuleiro.py
@@ -73,18 +73,22 @@ class Tabuleiro():
         self.clicou(0,0)
         self.botão0x0.configure(text=self.jogo.player)
         self.botão0x0.configure(state= "disabled")
+        .configure(font='Arial 100' )
+        self.botão0x0.configure(font='Arial 100' )
         self.jogo.recebe_jogada(0,0)
         
     def botão0x1_clicado(self):
         self.clicou(0,1)
         self.botão0x1.configure(text=self.jogo.player)
         self.botão0x1.configure(state= "disabled")
+        self.botão0x1.configure(font='Arial 100' )
         self.jogo.recebe_jogada(0,1)
         
     def botão0x2_clicado(self):
         self.clicou(0,2)
         self.botão0x2.configure(text=self.jogo.player)
-        self.botão0x2.configure(state= "disabled")  
+        self.botão0x2.configure(state= "disabled")
+        self.botão0x2.configure(font='Arial 100' )
         self.jogo.recebe_jogada(0,2)
 
 ################          
@@ -93,18 +97,21 @@ class Tabuleiro():
         self.clicou(1,0)
         self.botão1x0.configure(text=self.jogo.player)
         self.botão1x0.configure(state= "disabled")
+        self.botão1x0.configure(font='Arial 100' )
         self.jogo.recebe_jogada(1,0)
         
     def botão1x1_clicado(self):
         self.clicou(1,1)
         self.botão1x1.configure(text=self.jogo.player)
         self.botão1x1.configure(state= "disabled")
+        self.botão1x1.configure(font='Arial 100' )
         self.jogo.recebe_jogada(1,1)
         
     def botão1x2_clicado(self):
         self.clicou(1,2)
         self.botão1x2.configure(text=self.jogo.player)
         self.botão1x2.configure(state= "disabled")
+        self.botão1x2.configure(font='Arial 100' )
         self.jogo.recebe_jogada(1,2)
             
 ################            
@@ -113,18 +120,21 @@ class Tabuleiro():
         self.clicou(2,0)
         self.botão2x0.configure(text=self.jogo.player)
         self.botão2x0.configure(state= "disabled")
+        self.botão2x0.configure(font='Arial 100' )
         self.jogo.recebe_jogada(2,0)
         
     def botão2x1_clicado(self):
         self.clicou(2,1)
         self.botão2x1.configure(text=self.jogo.player)
         self.botão2x1.configure(state= "disabled")
+        self.botão2x1.configure(font='Arial 100' )
         self.jogo.recebe_jogada(2,1)
         
     def botão2x2_clicado(self):
         self.clicou(2,2)
         self.botão2x2.configure(text=self.jogo.player)
         self.botão2x2.configure(state= "disabled")
+        self.botão2x2.configure(font='Arial 100' )
         self.jogo.recebe_jogada(2,2)
 
 jogodavelha = Tabuleiro()
